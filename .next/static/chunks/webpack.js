eadf3d381f8dde92883c72786ce12913f3d88b3c
rajtilak-2020 <rajtilak1062020@gmail.com>
2025-02-03 19:45:14 +0530

    Update webpack hot-update files and modify app paths and build manifests

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2240bcb384accaba"; }
+/******/ 		__webpack_require__.h = function() { return "1fb42e0e22aab8cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,16 +1,66 @@
 import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
-//Hero Section Data
 export const DATA = {
-  name: "K Rajtilak's Portfolio",
-  initials: "KR",
-  url: "https://krajtilak.vercel.app",
-  location: "Odisha, India",
-  locationLink: "https://www.google.com/maps/place/india",
-  description: "A passionate Computer Science Engineering student, I strive to bridge the gap between innovative ideas and transformative technologies. I focus on crafting solutions that redefine possibilities.",
-  summary: "To leverage my skills in IoT, robotics, and programming to contribute effectively to innovative projects, while continuously enhancing my technical expertise and problem-solving abilities in a dynamic work environment.",
-  avatarUrl: "/me.png",
+
+    //navbar
+    navbar: [
+      { href: "/", icon: HomeIcon, label: "Home" },
+      { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    ],
+
+    contact: {
+      // email: "rajtilak1062020@gmail.com",
+      // tel: "+91 7008818173",
+  
+      social: {
+  
+        GitHub: {
+          name: "GitHub",
+          url: "https://github.com/rajtilak-2020",
+          icon: Icons.github,
+          navbar: true,
+        },
+  
+        LinkedIn: {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/krajtilak2020",
+          icon: Icons.linkedin,
+          navbar: true,
+        },
+  
+        X: {
+          name: "X",
+          url: "https://x.com/2020rajtilak",
+          icon: Icons.x,
+          navbar: true,
+        },
+  
+        Instagram: {
+          name: "Instagram",
+          url: "https://instagram.com/k.raj_tilak2020",
+          icon: Icons.instagram,
+          navbar: true,
+        },
+  
+        email: {
+          name: "Mail Me",
+          url: "mailto:rajtilak1062020@gmail.com",
+          icon: Icons.email,
+          navbar: true,
+        },
+      },
+    },
+
+    //Hero Section Data
+    name: "K Rajtilak's Portfolio",
+    initials: "KR",
+    url: "https://krajtilak.vercel.app",
+    location: "Odisha, India",
+    locationLink: "https://www.google.com/maps/place/india",
+    description: "A passionate Computer Science Engineering student, I strive to bridge the gap between innovative ideas and transformative technologies. I focus on crafting solutions that redefine possibilities.",
+    summary: "To leverage my skills in IoT, robotics, and programming to contribute effectively to innovative projects, while continuously enhancing my technical expertise and problem-solving abilities in a dynamic work environment.",
+    avatarUrl: "/me.png",
 
   //skills section
   skills: [
@@ -30,54 +80,8 @@ export const DATA = {
     "Raspberry Pi",
   ],
 
-  //navbar
-  navbar: [
-    { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
-  ],
 
-  contact: {
-    email: "rajtilak1062020@gmail.com",
-    tel: "+91 7008818173",
-
-    social: {
-
-      GitHub: {
-        name: "GitHub",
-        url: "https://github.com/rajtilak-2020",
-        icon: Icons.github,
-        navbar: true,
-      },
-
-      LinkedIn: {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/krajtilak2020",
-        icon: Icons.linkedin,
-        navbar: true,
-      },
-
-      X: {
-        name: "X",
-        url: "https://x.com/2020rajtilak",
-        icon: Icons.x,
-        navbar: true,
-      },
-
-      Instagram: {
-        name: "Instagram",
-        url: "https://instagram.com/k.raj_tilak2020",
-        icon: Icons.instagram,
-        navbar: true,
-      },
-
-      email: {
-        name: "Mail Me",
-        url: "mailto:rajtilak1062020@gmail.com",
-        icon: Icons.email,
-        navbar: true,
-      },
-    },
-  },
+ 
 
   work: [
     {
