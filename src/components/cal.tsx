c95b933ca76bbec28ef1e92fbed1a78681d27a3d
rajtilak-2020 <rajtilak1062020@gmail.com>
2025-06-01 20:08:47 +0530

    Fix syntax error in ScheduleCallFloat component and clean up formatting

--- src/components/cal.tsx
+++ src/components/cal.tsx
@@ -18,7 +18,7 @@ export function ScheduleCallFloat() {
         hideEventTypeDetails: false,
         layout: "month_view",
       });
-    });
+    })();
   },);
   return (
     <Button
@@ -26,8 +26,7 @@ export function ScheduleCallFloat() {
       data-cal-namespace="k-rajtilak"
       data-cal-link="k-rajtilak"
       data-cal-config='{"layout":"month_view"}'
-      className="group/cal fixed z-50 md:bottom-5 bottom-20 right-5 w-12 h-12 rounded-full hover:px-4 hover:w-auto p-2 duration-300 transition-all ease-out dark:border-white/[0.2] dark:bg-transparent bg-background backdrop-blur-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
-    >
+      className="group/cal fixed z-50 md:bottom-5 bottom-20 right-5 w-12 h-12 rounded-full hover:px-4 hover:w-auto p-2 duration-300 transition-all ease-out dark:border-white/[0.2] dark:bg-transparent bg-background backdrop-blur-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
       <div className="flex items-center gap-2">
         <p className="group-hover/cal:block hidden duration-300 transition-all ease-out">
           Schedule Meeting
