dde21858ab19e255459e47a983399f256956d19a
rajtilak-2020 <rajtilak1062020@gmail.com>
2025-07-28 00:46:45 +0530

    fix internal issues

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://krajtilak.vercel.app',
-  generateRobotsTxt: false,
+  generateRobotsTxt: true,
   sitemapSize: 5000,
   generateIndexSitemap: true,
 };
